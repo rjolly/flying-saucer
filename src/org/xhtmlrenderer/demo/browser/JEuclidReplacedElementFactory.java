efd6576370506b6a764edab54b3f37bf40c74966
Raphael Jolly <rjolly@users.sourceforge.net>
2020-06-14 13:58:33 +0200

    empty

--- src/org/xhtmlrenderer/demo/browser/JEuclidReplacedElementFactory.java
+++ src/org/xhtmlrenderer/demo/browser/JEuclidReplacedElementFactory.java
@@ -5,21 +5,48 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
+import java.io.Writer;
+import java.io.StringWriter;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.dom.DOMSource;
 import org.w3c.dom.Document;
 import net.sourceforge.jeuclid.DOMBuilder;
+import net.sourceforge.jeuclid.MathMLParserSupport;
 import net.sourceforge.jeuclid.MutableLayoutContext;
 import net.sourceforge.jeuclid.context.LayoutContextImpl;
 import net.sourceforge.jeuclid.context.Parameter;
 import net.sourceforge.jeuclid.layout.JEuclidView;
 
 public class JEuclidReplacedElementFactory extends IconReplacedElementFactory {
+	private final Transformer transformer;
+
 	public JEuclidReplacedElementFactory() {
 		super("math");
+		try {
+			transformer = TransformerFactory.newInstance().newTransformer(new StreamSource(getClass().getResource("/net/sourceforge/jeuclid/content/mathmlc2p.xsl").toString()));
+		} catch (final TransformerConfigurationException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private String c2p(final Document doc) {
+		final Writer writer = new StringWriter();
+		try {
+			transformer.transform(new DOMSource(doc), new StreamResult(writer));
+		} catch (final TransformerException e) {
+			e.printStackTrace();
+		}
+		return writer.toString().replaceAll("\u2148", "i");
 	}
 
 	public Image createImage(final Document doc) throws Exception {
 		((MutableLayoutContext) LayoutContextImpl.getDefaultLayoutContext()).setParameter(Parameter.SCRIPTMINSIZE, new Float(10f));
-		final JEuclidView view = DOMBuilder.getInstance().createJeuclidDom(doc).getDefaultView();
+		final JEuclidView view = DOMBuilder.getInstance().createJeuclidDom(MathMLParserSupport.parseString(c2p(doc))).getDefaultView();
 		final int width = (int) Math.ceil(view.getWidth());
 		final int height = (int) (Math.ceil(view.getAscentHeight()) + Math.ceil(view.getDescentHeight()));
 
